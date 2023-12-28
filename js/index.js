dcb31cfae6431765f8bba83349ca2db84df7212b
abdulalh-al-mohammad <abdullahalmuhammad6@mail.com>
2023-12-28 17:35:36 +0600

    add js header search bar

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.getElementById("search").addEventListener("click", function(){
+    console.log('hello');
+    const input = document.getElementsByClassName("form-control");
+    console.log(input);
+})
