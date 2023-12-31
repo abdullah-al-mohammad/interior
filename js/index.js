e147540e2ad925407fe37655166a68950b31738a
abdulalh-al-mohammad <abdullahalmuhammad6@mail.com>
2023-12-31 17:17:39 +0600

    add body button

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-document.getElementById("search").addEventListener("click", function(){
-    const inputContainer = document.getElementById("form");
-    const inputField = document.createElement('input');
-    console.log(inputField);
-    inputField.innerHTML = `
-    <input type="search" placeholder="Search" aria-label="Search">
-    `;
-    inputContainer.appendChild(inputField);
-})
+// document.getElementById("search").addEventListener("click", function(){
+//     const inputContainer = document.getElementById("form");
+//     const inputField = document.createElement('input');
+//     console.log(inputField);
+//     inputField.innerHTML = `
+//     <input type="search" placeholder="Search" aria-label="Search">
+//     `;
+//     inputContainer.appendChild(inputField);
+// })
 
 
-const home = document.getElementById('home');
-const event = home.addEventListener('click',function(){
-    console.log('Let Your Home Be Unique')
-})
+// const home = document.getElementById('home');
+// const event = home.addEventListener('click',function(){
+//     console.log('Let Your Home Be Unique')
+// })
